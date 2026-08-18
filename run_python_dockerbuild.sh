11d53769c6dc55681d13e7620e2602006c0395f5
吴小白 <296015668@qq.com>
2026-08-18 12:35:55 +0800

    feat: 更新补丁

--- run_python_dockerbuild.sh
+++ run_python_dockerbuild.sh
@@ -31,7 +31,7 @@ fi
 cd onnxruntime || exit 1
 if [ ! -f "cmake/vcpkg-ports/cpuinfo/patch_cpuinfo_h_for_loong64.patch" ]; then
   wget -qO - https://github.com/loong64/onnxruntime/raw/refs/heads/main/patch_loong64.patch | patch -p1
-  wget -qO cmake/vcpkg-ports/cpuinfo/patch_cpuinfo_h_for_loong64.patch https://github.com/loong64/cpuinfo/commit/e73f86b6404d99f9e52432ccbd58a6b4f24fc4a5.patch
+  wget -qO cmake/vcpkg-ports/cpuinfo/patch_cpuinfo_h_for_loong64.patch https://github.com/loong64/cpuinfo/commit/6d35f4b909c0c975eebd5e1d000b694762b48d0e.patch
 fi
 
 mkdir -p wheelhouse
